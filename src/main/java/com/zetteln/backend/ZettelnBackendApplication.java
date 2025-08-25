0eeb6dcb11dc39a70d87e5d98c34b0d1acca474e
Eli Teófilo <teofiloeli@gmail.com>
2025-08-25 20:24:52 -0300

    Starting project

---
+++ src/main/java/com/zetteln/backend/ZettelnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zetteln.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZettelnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZettelnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetteln/backend/ZettelnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetteln.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZettelnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
