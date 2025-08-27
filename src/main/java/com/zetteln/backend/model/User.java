f5ff8051a3fecbec5414961ba1682f9a47391662
Elissandro Vieira | casa <teofiloeli@gmail.com>
2025-08-27 05:56:51 -0300

    Adding data base Entity to User model

--- src/main/java/com/zetteln/backend/model/User.java
+++ src/main/java/com/zetteln/backend/model/User.java
@@ -1,9 +1,29 @@
 package com.zetteln.backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String passwordHash;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String passwordHash) {
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
 }
