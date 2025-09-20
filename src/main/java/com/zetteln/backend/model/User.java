75394004a8805225c2c4eeb188d5595ce5d36428
Eli Teófilo <teofiloeli@gmail.com>
2025-09-20 12:49:13 -0300

    Adding User Entity

--- src/main/java/com/zetteln/backend/model/User.java
+++ src/main/java/com/zetteln/backend/model/User.java
@@ -1,7 +1,13 @@
 package com.zetteln.backend.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
